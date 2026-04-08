02a17f7038f537b11ed72e479d34ca0c160224a4
Bond Inyang <amenofem2019@gmail.com>
2026-04-08 02:36:25 +0100

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<!-- Header -->
+  <header class="header">
+    <div class="header-inner">
+      <a href="index.html" class="logo">
+        <div class="logo-icon"><i class="fa-solid fa-droplet"></i></div>
+        <div class="logo-text">
+          Diamond Shine
+          <span>Auto Wash</span>
+        </div>
+      </a>
+      <button class="nav-toggle" aria-label="Toggle menu" aria-expanded="false"><span></span><span></span><span></span></button>
+      <nav class="nav">
+        <a href="index.html" class="nav-link active">Home</a>
+        <a href="about.html" class="nav-link">About</a>
+        <a href="services.html" class="nav-link">Services</a>
+        <a href="contact.html" class="nav-link">Contact</a>
+      </nav>
+      <a href="contact.html" class="header-cta">Visit Us Today</a>
+    </div>
+  </header>
